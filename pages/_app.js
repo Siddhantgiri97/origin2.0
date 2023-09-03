4a7858b317b6356615b7a715cc6eafe988be1821
Siddhant <siddhantgiri97@gmail.com>
2023-09-03 20:11:34 +0530

    s

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config, dom } from '@fortawesome/fontawesome-svg-core';
 import Navbar from './components/navbar'
@@ -8,12 +9,12 @@ import '@/styles/globals.css'
 
 
 config.autoAddCss = false;
-const GlobalStyles = createGlobalStyle`${dom.css()}`;
+
 
 export default function App({ Component, pageProps }) {
 
   return <>
-    <GlobalStyles />
+    
     <Navbar />
     <Component {...pageProps} />
     <Footer />
