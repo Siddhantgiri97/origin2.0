f7da9d5952c462e40c00162592077af007fbf178
Siddhant <siddhantgiri97@gmail.com>
2023-09-03 19:47:49 +0530

    icons size

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,15 @@ import Footer from './components/footer'
 import 'bootstrap/dist/css/bootstrap.css';
 import '@/styles/globals.css'
 
+import { config, dom } from '@fortawesome/fontawesome-svg-core';
+
+config.autoAddCss = false;
+const GlobalStyles = createGlobalStyle`${dom.css()}`;
+
 export default function App({ Component, pageProps }) {
 
   return <>
+    <GlobalStyles />
     <Navbar />
     <Component {...pageProps} />
     <Footer />
