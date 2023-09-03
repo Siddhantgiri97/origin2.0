762ddbda624b9dc7117a29c2809ae2dc7beae24c
Siddhant <siddhantgiri97@gmail.com>
2023-09-03 20:01:40 +0530

    icon sizes

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config, dom } from '@fortawesome/fontawesome-svg-core';
 import Navbar from './components/navbar'
 import Footer from './components/footer'
 import 'bootstrap/dist/css/bootstrap.css';
 import '@/styles/globals.css'
 
-import { config, dom } from '@fortawesome/fontawesome-svg-core';
+
 
 config.autoAddCss = false;
 const GlobalStyles = createGlobalStyle`${dom.css()}`;
