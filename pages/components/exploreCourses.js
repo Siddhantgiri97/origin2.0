3f05830fe38a74eb6023894a403ef647a399c027
Siddhant <siddhantgiri97@gmail.com>
2023-09-05 12:12:38 +0530

    apos set

--- pages/components/exploreCourses.js
+++ pages/components/exploreCourses.js
@@ -12,7 +12,7 @@ const ExploreCourses = () => {
                             <div className="text-center my-5">
                                 <h2 className="display-5 fw-bolder"><span className="text-gradient d-inline">Unlock your Potential</span></h2>
                                 <p className="lead fw-light mb-4">Embark on your journey to success with us.</p>
-                                <p className="text-muted">We&apos;re committed to shaping your future. We understand that the journey to cracking JEE Main, JEE Advanced, and MHT-CET can be challenging, but it's also incredibly rewarding. That's why we've designed courses that not only simplify complex subjects but also ignite a passion for learning.</p>
+                                <p className="text-muted">We&apos;re committed to shaping your future. We understand that the journey to cracking JEE Main, JEE Advanced, and MHT-CET can be challenging, but it&apos;s also incredibly rewarding. That&apos;s why we&apos;ve designed courses that not only simplify complex subjects but also ignite a passion for learning.</p>
                                 <div className={`${styles.centerButton} mt-5 pt-3`}>
                                     <Link href="/courses" className={`${styles.buttons}`}>Explore Courses</Link>
                                 </div>
