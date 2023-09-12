4841363e9e967f7d476437fbd0aea15a8a51c701
Siddhant <siddhantgiri97@gmail.com>
2023-09-12 11:10:00 +0530

    image added

--- pages/components/achievements.js
+++ pages/components/achievements.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 import CarouselItem from './carouselItem'
-import AmitSirImg from '../../public/Amit_sir1.png'
-import ShriSirImg from '../../public/Shrikant_sir1.png'
-import ShamSirImg from '../../public/shyamal_sir1.png'
+import VirtiImg from '../../public/studentImages/virti.png'
+import YashImg from '../../public/studentImages/yash.jpg'
+import SnehaImg from '../../public/studentImages/sneha.jpg'
+import ShamalImg from '../../public/studentImages/shamal.png'
+import HarshImg from '../../public/studentImages/harsh.jpg'
+import ManaliImg from '../../public/studentImages/manali.jpg'
+import RudraImg from '../../public/studentImages/rudra.jpg'
+import PrajwalImg from '../../public/studentImages/prajwal.jpg'
+import MujImg from '../../public/studentImages/mujtaba.png'
+import RamImg from '../../public/studentImages/ram.png'
+import ShraddhaImg from '../../public/studentImages/shraddha.png'
+import RahulImg from '../../public/studentImages/rahul.png'
+import ShreyaImg from '../../public/studentImages/shreya.png'
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel';
 
@@ -21,12 +34,60 @@ const Achievements = () => {
             <section className={`${styles.pageSection} bg-light py-3`} id={styles.portfolio}>
               <div className="container">
                 <div className="row">
-                  <CarouselItem studentImage={ShamSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={ShamSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={ShamSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={ShamSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={ShamSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={ShamSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={VirtiImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>IIT Bombay</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Virti Mehta</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={YashImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Trichy</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Yash Sirsat</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={SnehaImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Warangal</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Sneha Buktare</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={ShamalImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>VNIT Nagpur</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Shamal Telangade</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={HarshImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Rourkela</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Harshal Nage</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={ManaliImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>VNIT Nagpur</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Manali Kulkarni</div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </section>
@@ -35,12 +96,66 @@ const Achievements = () => {
             <section className={`${styles.pageSection} bg-light py-3`} id={styles.portfolio}>
               <div className="container">
                 <div className="row">
+                  {/* <CarouselItem studentImage={ShriSirImg} studentCollege={"NIT Jaipur"} studentCity={"Rudrayani Adichwal"} />
+                  <CarouselItem studentImage={ShriSirImg} studentCollege={"NIT Bhopal"} studentCity={"Prajwal Naphade"} /> */}
+                  {/* <CarouselItem studentImage={ShriSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
                   <CarouselItem studentImage={ShriSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
                   <CarouselItem studentImage={ShriSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={ShriSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={ShriSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={ShriSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={ShriSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
+                  <CarouselItem studentImage={ShriSirImg} studentCollege={"MIT"} studentCity={"Pune"} /> */}
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={RudraImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Jaipur</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Rudrayani Adichwal</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={PrajwalImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Bhopal</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Prajwal Naphade</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={RamImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Trichy</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Ram Sakarkar</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={ShraddhaImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Surat</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Shraddha Sidurkar</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={MujImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT A.P</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Mujtaba Ansari</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={ShreyaImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Nagpur</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Shreya Dhote</div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </section>
@@ -49,12 +164,67 @@ const Achievements = () => {
             <section className={`${styles.pageSection} bg-light py-3`} id={styles.portfolio}>
               <div className="container">
                 <div className="row">
+                  {/* <CarouselItem studentImage={AmitSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
                   <CarouselItem studentImage={AmitSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
                   <CarouselItem studentImage={AmitSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
                   <CarouselItem studentImage={AmitSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
                   <CarouselItem studentImage={AmitSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={AmitSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
-                  <CarouselItem studentImage={AmitSirImg} studentCollege={"MIT"} studentCity={"Pune"} />
+                  <CarouselItem studentImage={AmitSirImg} studentCollege={"MIT"} studentCity={"Pune"} /> */}
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={RahulImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT A.P</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Rahul Madhwani</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={RudraImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Jaipur</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Rudrayani Adichwal</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={RudraImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Jaipur</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Rudrayani Adichwal</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={RudraImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Jaipur</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Rudrayani Adichwal</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={RudraImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Jaipur</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Rudrayani Adichwal</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-6 mb-4">
+                    <div className={styles.portfolioItem}>
+                      <Image className="img-fluid" src={RudraImg} alt="..." />
+                      <div className={`${styles.portfoliocaption} shadow`} >
+                        <div className={styles.portfoliocaptionheading}>NIT Jaipur</div>
+                        <div className={`${styles.portfoliocaptionsubheading} text-muted`}>Rudrayani Adichwal</div>
+                      </div>
+                    </div>
+                  </div>
+
                 </div>
               </div>
             </section>
--- pages/components/exploreCourses.js
+++ pages/components/exploreCourses.js
@@ -6,6 +6,7 @@ import { faBookOpenReader } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faBookOpenReader);
 import styles from '@/styles/Home.module.css'
+import CoursesCard from './coursesCard';
 
 const ExploreCourses = () => {
     return (
@@ -18,75 +19,8 @@ const ExploreCourses = () => {
                                 <h2 className="display-5 fw-bolder"><span className="text-gradient d-inline">Unlock your Potential</span></h2>
                                 <p className="lead fw-light mb-4">Embark on your journey to success with us.</p>
                                 <p className="text-muted">We&apos;re committed to shaping your future. We understand that the journey to cracking JEE Main, JEE Advanced, and MHT-CET can be challenging, but it&apos;s also incredibly rewarding. That&apos;s why we&apos;ve designed courses that not only simplify complex subjects but also ignite a passion for learning.</p>
-                                <div className={styles.section_our_solution}>
-                                    <div className="row pt-5">
-                                        <div className="col-lg-4 col-md-12 col-sm-12">
-                                            <div className={styles.our_solution_category}>
-                                                <div className={styles.solution_cards_box}>
-                                                    <div className={`${styles.solution_card}`}>
-                                                        <div className={`${styles.so_top_icon} text-center pt-4`}>
-                                                            <FontAwesomeIcon icon={faBookOpenReader} className={styles.fontIcon} />
-                                                        </div>
-                                                        <div className={styles.solu_title}>
-                                                            <div>IIT-JEE</div>
-                                                        </div>
-                                                        <div className={styles.solu_description}>
-                                                            <p>
-                                                                It is a long established fact that a reader will be distracted by the readable
-                                                                content of a page when looking at its layout.
-                                                            </p>
+                                <CoursesCard />
 
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-4 col-md-12 col-sm-12">
-                                            <div className={styles.our_solution_category}>
-                                                <div className={styles.solution_cards_box}>
-                                                    <div className={styles.solution_card}>
-
-                                                        <div className={`${styles.so_top_icon} text-center pt-4`}>
-                                                            <FontAwesomeIcon icon={faBookOpenReader} className={styles.fontIcon} />
-                                                        </div>
-                                                        <div className={styles.solu_title}>
-                                                            <div>MHT-CET</div>
-                                                        </div>
-                                                        <div className={styles.solu_description}>
-                                                            <p>
-                                                                It is a long established fact that a reader will be distracted by the readable
-                                                                content of a page when looking at its layout.
-                                                            </p>
-
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-4 col-md-12 col-sm-12">
-                                            <div className={styles.our_solution_category}>
-                                                <div className={styles.solution_cards_box}>
-                                                    <div className={styles.solution_card}>
-
-                                                        <div className={`${styles.so_top_icon} text-center pt-4`}>
-                                                            <FontAwesomeIcon icon={faBookOpenReader} className={styles.fontIcon} />
-                                                        </div>
-                                                        <div className={styles.solu_title}>
-                                                            <div>NEET</div>
-                                                        </div>
-                                                        <div className={styles.solu_description}>
-                                                            <p>
-                                                                It is a long established fact that a reader will be distracted by the readable
-                                                                content of a page when looking at its layout.
-                                                            </p>
-
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
                                 <div className={` mt-5 pt-3`}>
                                     <Link href="/courses" className={`${styles.buttons}`}>Explore Courses</Link>
                                 </div>
